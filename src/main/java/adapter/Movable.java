8c93fff323117f0e7c9002259209715dbb372ae5
Hoang Phan <51194076+hoangytm@users.noreply.github.com>
2022-04-03 17:13:44 +0700

    add adapter

--- src/main/java/adapter/Movable.java
+++
@@ -1,6 +0,0 @@
-package adapter;
-
-public interface Movable {
-    // returns speed in MPH 
-    double getSpeed();
-}
--- src/main/java/adapter/MovableAdapter.java
+++
@@ -1,6 +0,0 @@
-package adapter;
-
-public interface MovableAdapter {
-    // returns speed in KM/H 
-    double getSpeed();
-}
